cb2d0d2d17e034b12e3574a10813369c4b32a7e7
AbdouBeny <benyoucefabderazak@gmail.com>
2025-12-02 22:57:40 +0100

    init e-bazar

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * On indique que les chemins des fichiers qu'on inclut
+ * seront relatifs au répertoire src.
+ */
+set_include_path("./src");
+
+/* Inclusion des classes utilisées dans ce fichier */
+require_once("Router.php");
+
+/*
+ * Cette page est simplement le point d'arrivée de l'internaute
+ * sur notre site.
+ */
+$router = new Router();
+$router->main();
+?>
